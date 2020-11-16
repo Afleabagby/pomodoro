924b408ff0443c06f93182388f8a773a8b518c54
Afleabagby <arby70317@gmail.com>
2020-11-16 09:22:00 +0800

    App.vue 內容

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
 <template lang="pug">
   #app
-  </div>
+    b-navbar(toggleable='lg' type='dark' variant='info')
+      b-container
+        b-navbar-brand(to='/') 番茄鐘
+        b-navbar-toggle(target='nav-collapse')
+        b-collapse#nav-collapse(is-nav)
+          b-navbar-nav
+            //- https://bootstrap-vue.org/docs/components/nav#comp-ref-b-nav-item
+            b-nav-item(to='/list' active-class="active") 待辦
+            b-nav-item(to='/settings' active-class="active") 設定
+    router-view
 </template>
